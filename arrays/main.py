ed7c1d7431cccc1dca12fef9179f3b711017ddc8
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2025-07-15 00:40:00 -0500

    UPDATED test code

--- arrays/main.py
+++ arrays/main.py
@@ -4,4 +4,21 @@ from DVD_Array_File import DVDArray
 
 #Here we create some DVDS 
 dvd1 = DVD("The Matrix", "Sci-Fi", 1999)
-dvd2
+dvd2 = DVD("Friday Night Lights", "Drama", 2004)
+dvd3 = DVD("Straw", "Drama", 2025)
+dvd4 = DVD("Hackers", "Thriller", 1995)
+
+#Create a DvD array (shelf) with a capacity of 5
+shelf = DVDArray(5)
+
+# Insert the DvDs
+shelf.insert(0, dvd1)
+shelf.insert(1, dvd2)
+shelf.insert(2, dvd3)
+shelf.insert(3, dvd4)
+
+#Print the full shelf
+print(shelf)
+
+#Get and print a specific DVD
+print 
