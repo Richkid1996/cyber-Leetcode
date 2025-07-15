54669db14486f5102959fd610e78b26a7aa8406f
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2025-07-14 23:35:10 -0500

    ADDING the script to run and test our Array

---
+++ arrays/main.py
@@ -0,0 +1,7 @@
+#Script to run and test your DVD array 
+from DVD_definition import DVD
+from DVD_Array_File import DVDArray 
+
+#Here we create some DVDS 
+dvd1 = DVD("The Matrix", "Sci-Fi", 1999)
+dvd2
